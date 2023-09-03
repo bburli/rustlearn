2d8e2e1aa013c0fea1333245cebb89fee4fb87b7
Badarinarayan Burli <badarinarayanburli@Madhuris-MacBook-Air.local>
2023-09-03 12:22:37 +0530

    Learning Part 1: Minor changes

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ fn main() {
     io::stdin().read_line(&mut input).unwrap();
     let earth_weight = input.trim().parse().unwrap();
 
-    println!("Weight on mars is {} kg", calc_weight_on_mars(earth_weight)); // need to reduce weight!
+    println!("Weight on mars is {} kg", calc_weight_on_mars(earth_weight));
 }
 
-fn calc_weight_on_mars(_weight: f32) -> f32 {
-    (_weight / 9.81) * 3.711
+fn calc_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711 // return can be skipped if it's an expression
 }
